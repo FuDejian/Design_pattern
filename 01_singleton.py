3882b75a225535b6dc81cb18b8ddbc7f96df0fe9
FuDejian <fudejian1008@163.com>
2021-08-25 23:40:32 +0800

    01_单例模式_2

--- 01_singleton.py
+++ 01_singleton.py
@@ -4,14 +4,16 @@
 class MySingleton:
 
     # 类属性
-    __obj = None
+    __instance = None
     __init_flag = True
 
     def __new__(cls, *args, **kwargs):
-        if cls.__obj is None:
-            cls.__obj = object.__new__(cls)
-        return cls.__obj
+        if cls.__instance is None:
+            # cls.__instance = object.__new__(cls)
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
 
+    # __init__ 方法是否需要重复执行，需要看实际情况
     def __init__(self, name):
         if MySingleton.__init_flag:
             print("init.....")
